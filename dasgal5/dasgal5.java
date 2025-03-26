f19a862a65e629b05321d76ff7a1f7eeae90a0c8
Jawhlan116 <jawkhlan2005@gmail.com>
2025-03-26 14:19:06 +0800

    Zunii saruud

---
+++ dasgal5/dasgal5.java
@@ -0,0 +1,12 @@
+package lab9;
+
+public class dasgal5 {
+
+	public static void main(String[] args) {
+		String[] zun = {"Тавдугаар сар ","Зургадугаар сар ","Долоодугаар сар ","Наймдугаар сар "};
+		assert zun.length==3:"Зуны 3 сар байдаг.";
+		System.out.println("Энэ бол зуны сарууд:");
+		System.out.println(zun[0]+zun[1]+zun[2]+zun[3]);
+	}
+
+}
